5a74e51a57b8b902466b585cf1dc7e347d4367c6
farthjun <52003109+farthjun@users.noreply.github.com>
2019-08-12 11:27:09 +0800

    Update featureMatch(1).py
    去掉了之前尝试的部分，保留了最终版，稍微美化了一下格式

--- featureMatch(1).py
+++ featureMatch(1).py
@@ -9,106 +9,43 @@ def get_grades(value):
         grade = 100
     return grade
 
-def get_values():
-	path = 'hands\\hand'
-	for i in range(1,13):
-		for j in range(i+1,14):
-			path1=path+ str(i) +'.png'
-			path2=path+ str(j) +'.png'
-			print('hand'+str(i)+' to '+'hand'+str(j)+':')
-			img1 = cv2.imread(path1)
-			img2 = cv2.imread(path2)
-			img_gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)#转为灰度图像
-			img_gray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
-			print(cv2.contourArea(img_gray1))
-			print('match  = ', cv2.matchShapes(img_gray1, img_gray2, 1, 0.0))
-			print('=========================================================')
-
-def featureMatchP():
-	path='hands1\\hand'
-	for i in range(1,13):
-		for j in range(i+1,14):
-			path1=path+str(i)+'.png'
-			path2=path+str(j)+'.png'
-			hand1=cv2.imread(path1)
-			grayScale1=cv2.cvtColor(hand1,cv2.IMREAD_GRAYSCALE)
-			hand2=cv2.imread(path2)
-			grayScale2=cv2.cvtColor(hand2,cv2.IMREAD_GRAYSCALE)
-			#展示原图
-			plt.figure()
-			plt.axis('off')
-			plt.imshow(hand1)
-			plt.figure()
-			plt.axis('off')
-			plt.imshow(hand2)
-			#plt.show()
-			#特征提取
-			orb=cv2.ORB_create()
-			kp1, des1 = orb.detectAndCompute(grayScale1, None)
-			kp2, des2 = orb.detectAndCompute(grayScale2, None)
-			#可视化特征点
-			keypoint1 = cv2.drawKeypoints(image=hand1, outImage=hand1,keypoints=kp1,
-		                              flags=4, color=(0,255,0))
-			keypoint2 = cv2.drawKeypoints(image=hand2, outImage=hand2,keypoints=kp2,
-		                              flags=4, color=(0,255,0))
-			plt.figure()
-			plt.axis('off')
-			plt.imshow(keypoint1)
-			plt.figure()
-			plt.axis('off')
-			plt.imshow(keypoint2)
-			#plt.show()
-			#匹配：遍历两幅图像的特征描述符[包含特征点信息的向量]然后计算向量之间的距离
-			bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-			matches = bf.match(des1,des2)
-			matches = sorted(matches, key = lambda x:x.distance)
-			avg = 0.0
-			for k in range(0,50):
-				avg=avg+matches[k].distance
-			print('hand'+str(i)+' to '+'hand'+str(j)+':')
-			print(len(matches))
-			avg=avg/len(matches)
-			print(avg)
-			print(get_grades(avg))
-			print('============================================')
-
 def featureMatch(pathA,pathB):
     standard=cv2.imread(pathA)
     img=cv2.imread(pathB)
     grayScale1=cv2.cvtColor(standard,cv2.IMREAD_GRAYSCALE)
     grayScale2=cv2.cvtColor(img,cv2.IMREAD_GRAYSCALE)
-	#展示原图q
+#   展示原图q
     plt.figure()
     plt.axis('off')
-#    plt.imshow(hand1)
+#   plt.imshow(hand1)
     plt.figure()
     plt.axis('off')
-#    plt.imshow(hand2)
-	#plt.show()
-	#特征提取
+#   plt.imshow(hand2)
+#   plt.show()
+#   特征提取
     orb=cv2.ORB_create()
     kp1, des1 = orb.detectAndCompute(grayScale1, None)
     kp2, des2 = orb.detectAndCompute(grayScale2, None)
     print(type(des1))
     print(type(des2))
-	#可视化特征点
-#    keypoint1 = cv2.drawKeypoints(image=hand1, outImage=hand1,keypoints=kp1,flags=4, color=(0,255,0))
-#    keypoint2 = cv2.drawKeypoints(image=hand2, outImage=hand2,keypoints=kp2,flags=4, color=(0,255,0))
+#   可视化特征点
+#   keypoint1 = cv2.drawKeypoints(image=hand1, outImage=hand1,keypoints=kp1,flags=4, color=(0,255,0))
+#   keypoint2 = cv2.drawKeypoints(image=hand2, outImage=hand2,keypoints=kp2,flags=4, color=(0,255,0))
     plt.figure()
     plt.axis('off')
-#    plt.imshow(keypoint1)
+#   plt.imshow(keypoint1)
     plt.figure()
     plt.axis('off')
-#    plt.imshow(keypoint2)
-	#plt.show()
-	#匹配：遍历两幅图像的特征描述符[包含特征点信息的向量]然后计算向量之间的距离
+#   plt.imshow(keypoint2)
+#   plt.show()
+#   匹配：遍历两幅图像的特征描述符[包含特征点信息的向量]然后计算向量之间的距离
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
     matches = bf.match(des1,des2)
     matches = sorted(matches, key=lambda x:x.distance)
     avg = 0.0
     for k in range(0,60):
         avg=avg+matches[k].distance
-#    print('hand'+str(i)+' to '+'hand'+str(j)+':')
+#   print('hand'+str(i)+' to '+'hand'+str(j)+':')
     avg=avg/len(matches)
     c=get_grades(avg)
     print('Your grade is:',c)
@@ -119,8 +56,8 @@ bs = cv2.createBackgroundSubtractorKNN(detectShadows=True)
 es = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 while True:
     grabbed, frame_lwpCV = camera.read()
-#    res_b = do_contours('hands1\\hand4.png')
-#    print('res_b %d contours' % len(res_b))
+#   res_b = do_contours('hands1\\hand4.png')
+#   print('res_b %d contours' % len(res_b))
     fgmask = bs.apply(frame_lwpCV) # 背景分割器，该函数计算了前景掩码
     # 二值化阈值处理，前景掩码含有前景的白色值以及阴影的灰色值，在阈值化图像中，将非纯白色（244~255）的所有像素都设为0，而不是255
     th = cv2.threshold(fgmask.copy(), 244, 255, cv2.THRESH_BINARY)[1]
